74d2d412eaaf6f0aa43d59913298cf1ba62715c3
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-05-14 09:07:04 +0200

    stzling established

--- showtime_frontend/src/App.vue
+++ showtime_frontend/src/App.vue
@@ -5,28 +5,51 @@ import MainTitle from '@/components/MainTitle.vue'
 
 <template>
   <header>
-    <img alt="Showtime-logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Showtime-logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
-    <div class="wrapper">
-      <MainTitle> </MainTitle>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <a href="/views/LoginScreen.vue">Login</a>
-      </nav>
-    </div>
+    <MainTitle> </MainTitle>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <a href="/login.html">Login</a>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-  nav a{
-    margin-right: 1rem;
-    color: #42b983;
-    font-weight: bold;
+nav a {
+  margin-right: 1rem;
+  color: var(--color-text);
+  font-weight: bold;
+  padding: 0.2rem 0.5rem;
+}
+nav {
+    display: flex;
+
+}
+header {
+
+  border-radius: 25px;
+  background-color: var(--color-heading);
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+  margin-top: 0.5rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 80%;
+    margin: 0.5rem inherit auto auto;
+
+    padding: 1rem 2rem;
   }
-  nav{
-    margin-top: 1rem;
-  }
+
+
+}
 </style>
--- showtime_frontend/src/login-main.ts
+++ showtime_frontend/src/login-main.ts
@@ -4,11 +4,9 @@ import { createApp } from 'vue'
 import { createPinia, getActivePinia } from 'pinia'
 
 import LoginPage from './LoginPage.vue'
-import router from './router'
 
 const app = createApp(LoginPage)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#login-app')
