d41927941589429e0f40699219c5421499d30706
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-07-08 21:18:36 +0200

    I messed up something in git

--- fullstack/components/ViewWorkshop.vue
+++ fullstack/components/ViewWorkshop.vue
@@ -1,24 +1,20 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-import type { Workshop } from "~/server/types/types";
+import type { Participant, Workshop } from "~/server/types/types";
+const toast = useToast();
 
-const town = ref("");
-const date = ref("");
-const open = ref(false);
-const location = ref("");
 const props = defineProps<{
   id: string;
 }>();
 
-const workshop: Ref<Workshop> = ref({
-  id: props.id,
+const state = ref({
   town: "",
-  time: "",
-  location: "",
+  time: Date.now().toString(),
   open: false,
-  participants: [],
-  teachers: [],
+  location: "",
+  participants: [] as Participant[],
 });
+const workshopid = ref('');
 async function refresh() {
   const response = await $fetch("/api/workshops", {
     method: "GET",
@@ -27,42 +23,56 @@ async function refresh() {
     },
   });
   const data = response;
-  workshop.value = data.find((w: Workshop) => w.id == props.id) || {
-    id: props.id,
-    town: "",
-    time: "",
-    location: "",
-    open: false,
-    participants: [],
-    teachers: [],
-  };
-  town.value = workshop.value.town;
-  location.value = workshop.value.location || "";
-  date.value = new Date(workshop.value.time).toISOString().slice(0, 16);
-  open.value = workshop.value.open;
-  console.log("Workshop data:", workshop.value);
+  const workshop = data.find((w: Workshop) => w.id == props.id);
+  if (!workshop) {
+    toast.add({
+      title: "Workshop not found",
+      color: "error",
+    });
+    return;
+  }
+  workshopid.value = workshop.id;
+  state.value.town = workshop.town;
+  state.value.time = workshop.time;
+  state.value.location = workshop.location ?? "";
+  state.value.open = workshop.open;
+  state.value.participants = workshop.participants || [];
+  console.log("Workshop data:", state.value);
 }
 
 async function submitWorkshop() {
-  const form = document.querySelector("form");
-  console.log(form);
-  if (!form) return;
-
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
+  const workshop = {
+    id: workshopid.value,
+    town: state.value.town,
+    time: state.value.time,
+    location: state.value.location,
+    open: state.value.open,
+    participants: state.value.participants,
+  };
 
   try {
-    console.log("Submitting workshop data:", data);
-    const response = await $fetch("/api/workshop", {
+    console.log("Submitting workshop data:", workshop);
+    await $fetch("/api/edit_workshop", {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(workshop),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    console.log("Workshop created:", response);
+    location.reload();
   } catch (error) {
-    console.error("Error creating workshop:", error);
+    toast.add({
+      title: (error as Error).message,
+      color: "error",
+    });
+  }
+}
+async function removeParticipant(email: string) {
+  if (window.confirm("Biztosan eltávolítod a résztvevőt?")) {
+
+    workshop.value.participants = workshop.value.participants.filter(
+      (p) => p.email !== email
+    );
   }
 }
 refresh();
@@ -72,47 +82,45 @@ refresh();
     <button class="button">Részletek</button>
 
     <template #body>
-      <form class="flex flex-col gap-4" @submit.prevent="submitWorkshop()">
-        <label for="town">Város</label>
-        <input
-          id="town"
-          v-model="town"
-          class=""
-          type="text"
-          name="town"
-          required
-        >
-        <label for="location">Helyszín</label>
-        <input
-          id="location"
-          v-model="location"
-          class=""
-          type="text"
-          name="location"
-        >
-        <label for="date">Dátum</label>
-        <input
-          id="date"
-          v-model="date"
-          type="datetime-local"
-          name="date"
-          required
-        >
-        <label for="participants">Résztvevők</label>
-        <ul
-          v-for="participant in workshop.participants"
-          id="participants"
-          :key="participant.email"
-        >
-          <li>{{ participant.name }} ({{ participant.email }})</li>
+      <UForm :schema="schema" :state="state" class="flex flex-col gap-4">
+        <UFormField>
+          <UInput v-model="state.town" label="Város" name="town" required />
+        </UFormField>
+        <UFormField>
+          <UInput v-model="state.location" label="Helyszín" name="location" />
+        </UFormField>
+        <UFormField>
+          <UInput
+            v-model="state.time"
+            label="Dátum"
+            name="date"
+            type="datetime-local"
+            required
+          />
+        </UFormField>
+        <UFormField>
+          <UCheckbox
+            v-model="state.open"
+            label="Nyílt foglalkozás"
+            name="open"
+          />
+        </UFormField>
+        <ul>
+          <li
+            v-for="participant in state.participants"
+            :key="participant.email"
+          >
+            {{ participant.name }} ({{ participant.email }})
+            <UButton
+              @click="
+                removeParticipant(participant.email)
+              "
+              >Eltávolítás</UButton
+            >
+          </li>
         </ul>
-
-        <div>
-          <label for="open" class="mr-4">Nyílt foglalkozás</label>
-          <input id="open" v-model="open" type="checkbox" name="open" />
-        </div>
-        <button type="submit" class="button">Mentés</button>
-      </form>
+        <UButton @click="submitWorkshop">Mentés</UButton>
+      </UForm>
     </template>
   </UModal>
 </template>
